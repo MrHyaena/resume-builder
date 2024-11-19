79b2b5da80832e76bc5481ec832de59ebc30eb8d
MrHyaena <dolezalmartin131@gmail.com>
2024-11-19 15:28:03 +0100

    Working controls, state for open/close cntr panel
    - I just copied logic from working education
    into work experience controls
    - Added buttons for opening and closing controls
    elements
    - Css

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,28 @@ const educObject = {
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe totam ad possimus! Modi ipsum deleniti sapiente ullam eveniet perspiciatis, neque fugit blanditiis unde amet culpa quam eaque nobis illum? Voluptatem!",
 };
 
+const workObject = {
+  id: 0,
+  company: "Foxdeli",
+  position: "Sales manager",
+  workStart: "2018",
+  workEnd: "2023",
+  workDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe totam ad possimus! Modi ipsum deleniti sapiente ullam eveniet perspiciatis, neque fugit blanditiis unde amet culpa quam eaque nobis illum? Voluptatem!",
+};
+
 // Application being put together
 
 export default function App() {
-  const [eduId, setEduId] = useState(1);
   const [personalInfo, setPersonalInfo] = useState(infoObject);
+
+  const [eduId, setEduId] = useState(1);
   const [education, setEducation] = useState([educObject]);
+
+  const [work, setWork] = useState([workObject]);
+  const [workId, setWorkId] = useState(1);
+
+  const [open, setOpen] = useState(0);
 
   function setName(e) {
     return setPersonalInfo({ ...personalInfo, name: e.target.value });
@@ -84,12 +100,63 @@ export default function App() {
     return;
   }
 
-  function deleteEducation() {}
+  function addWork() {
+    const company = document.getElementById("company");
+    const position = document.getElementById("position");
+    const workStart = document.getElementById("workStart");
+    const workEnd = document.getElementById("workEnd");
+    const workDescription = document.getElementById("workDescription");
+
+    setEducation([
+      ...education,
+      {
+        id: workId,
+        company: company.value,
+        position: position.value,
+        workStart: workStart.value,
+        workEnd: workEnd.value,
+        workDescription: workDescription.value,
+      },
+    ]);
+
+    setWorkId(eduId + 1);
+    company.value = "";
+    position.value = "";
+    workStart.value = "";
+    workEnd.value = "";
+    workDescription.value = "";
+
+    return;
+  }
+
+  function deleteEducation(id) {
+    const index = education.findIndex((item) => item.id === id);
+    if (index !== -1) {
+      const newArray = [
+        ...education.slice(0, index),
+        ...education.slice(index + 1),
+      ];
+      setEducation(newArray);
+    }
+
+    return;
+  }
+
+  function deleteWork(id) {
+    const index = work.findIndex((item) => item.id === id);
+    if (index !== -1) {
+      const newArray = [...work.slice(0, index), ...work.slice(index + 1)];
+      setWork(newArray);
+    }
+
+    return;
+  }
 
   return (
     <>
       <div className="controls">
         <h1 className="controlHead">Make your CV!</h1>
+
         <PersonalInfoControls
           setName={setName}
           setSurname={setSurname}
@@ -97,11 +164,25 @@ export default function App() {
           setPhone={setPhone}
           setAdressone={setAdressone}
           setAdresstwo={setAdresstwo}
+          open={open}
+          setOpen={setOpen}
         />
-        <h2 className="controlHead">Education</h2>
-        <EducationControls addEducation={addEducation} />
+
+        <EducationControls
+          addEducation={addEducation}
+          open={open}
+          setOpen={setOpen}
+        />
         <div className="itemsControl">
-          <EducationControlsDivs education={education} />
+          <EducationDivs
+            education={education}
+            deleteEducation={deleteEducation}
+          />
+        </div>
+
+        <WorkControls addWork={addWork} open={open} setOpen={setOpen} />
+        <div className="itemsControl">
+          <WorkDivs work={work} deleteWork={deleteWork} />
         </div>
       </div>
       <div className="board">
@@ -120,13 +201,16 @@ export default function App() {
           <div className="education">
             <CvEducation education={education} />
           </div>
+          <div className="education">
+            <CvWork work={work} />
+          </div>
         </div>
       </div>
     </>
   );
 }
 
-// Header information + controls
+// Controls
 
 function PersonalInfoControls({
   setName,
@@ -135,68 +219,226 @@ function PersonalInfoControls({
   setPhone,
   setAdressone,
   setAdresstwo,
+  open,
+  setOpen,
 }) {
-  return (
-    <>
-      <div className="controlsSection">
-        <label>
-          Your name
-          <input onChange={setName}></input>
-        </label>
-        <label>
-          Your surname
-          <input onChange={setSurname}></input>
-        </label>
-        <label>
-          Email
-          <input onChange={setEmail}></input>
-        </label>
-        <label>
-          Phone
-          <input onChange={setPhone}></input>
-        </label>
-        <label>
-          Adress line
-          <input onChange={setAdressone}></input>
-        </label>
-        <label>
-          Adress line 2<input onChange={setAdresstwo}></input>
-        </label>
-      </div>
-    </>
-  );
+  if (open !== 1) {
+    return (
+      <>
+        <div className="headerSection">
+          <h2 className="controlHead">Personal information</h2>
+          <button
+            id="infoBtn"
+            onClick={() => {
+              setOpen(1);
+            }}
+          >
+            Open
+          </button>
+        </div>
+      </>
+    );
+  } else if (open === 1) {
+    return (
+      <>
+        <div className="headerSection">
+          <h2 className="controlHead">Personal information</h2>
+          <button
+            id="infoBtn"
+            onClick={() => {
+              setOpen("");
+            }}
+          >
+            Close
+          </button>
+          <label>
+            Your name
+            <input onChange={setName}></input>
+          </label>
+          <label>
+            Your surname
+            <input onChange={setSurname}></input>
+          </label>
+          <label>
+            Email
+            <input onChange={setEmail}></input>
+          </label>
+          <label>
+            Phone
+            <input onChange={setPhone}></input>
+          </label>
+          <label>
+            Adress line
+            <input onChange={setAdressone}></input>
+          </label>
+          <label>
+            Adress line 2<input onChange={setAdresstwo}></input>
+          </label>
+        </div>
+      </>
+    );
+  }
 }
 
-function EducationControls({ addEducation }) {
-  return (
-    <>
-      <div className="controlsSection">
-        <label>
-          School Name
-          <input id="school"></input>
-        </label>
-        <label>
-          Program
-          <input id="program"></input>
-        </label>
-        <label>
-          Description
-          <textarea id="description"></textarea>
-        </label>
-        <label>
-          Starting Year
-          <input id="eduStart"></input>
-        </label>
-        <label>
-          Ending Year
-          <input id="eduEnd"></input>
-        </label>
+function EducationControls({ addEducation, open, setOpen }) {
+  if (open !== 2) {
+    return (
+      <>
+        <div className="headerSection">
+          <h2 className="controlHead">Education</h2>
+          <button
+            id="infoBtn"
+            onClick={() => {
+              setOpen(2);
+            }}
+          >
+            Open
+          </button>
+        </div>
+      </>
+    );
+  } else if (open == 2) {
+    return (
+      <>
+        <div className="headerSection">
+          <h2 className="controlHead">Education</h2>
+          <button
+            id="eduBtn"
+            onClick={() => {
+              setOpen("");
+            }}
+          >
+            Close
+          </button>
+          <label>
+            School Name
+            <input id="school"></input>
+          </label>
+          <label>
+            Program
+            <input id="program"></input>
+          </label>
+          <label>
+            Description
+            <textarea id="description"></textarea>
+          </label>
+          <label>
+            Starting Year
+            <input id="eduStart"></input>
+          </label>
+          <label>
+            Ending Year
+            <input id="eduEnd"></input>
+          </label>
 
-        <button onClick={addEducation}>Add</button>
-      </div>
-    </>
-  );
+          <button onClick={addEducation}>Add</button>
+        </div>
+      </>
+    );
+  }
 }
+
+function EducationDivs({ education, deleteEducation }) {
+  const educationarray = education.map((item, index) => {
+    return (
+      <div key={index} className="educationItemControls">
+        <h4>{item.school}</h4>
+
+        <button
+          className="educationDelete"
+          onClick={() => {
+            deleteEducation(item.id);
+          }}
+        >
+          ❌
+        </button>
+      </div>
+    );
+  });
+
+  return <>{educationarray}</>;
+}
+
+function WorkControls({ addWork, open, setOpen }) {
+  if (open !== 3) {
+    return (
+      <>
+        <div className="headerSection">
+          <h2 className="controlHead">Work experience</h2>
+          <button
+            id="infoBtn"
+            onClick={() => {
+              setOpen(3);
+            }}
+          >
+            Open
+          </button>
+        </div>
+      </>
+    );
+  } else if (open == 3) {
+    return (
+      <>
+        <div className="controlsSection">
+          <h2 className="controlHead">Work eperience</h2>
+          <button
+            id="eduBtn"
+            onClick={() => {
+              setOpen("");
+            }}
+          >
+            Close
+          </button>
+          <label>
+            Company Name
+            <input id="company"></input>
+          </label>
+          <label>
+            Position
+            <input id="position"></input>
+          </label>
+          <label>
+            Description
+            <textarea id="workDescription"></textarea>
+          </label>
+          <label>
+            Starting Year
+            <input id="workStart"></input>
+          </label>
+          <label>
+            Ending Year
+            <input id="workEnd"></input>
+          </label>
+
+          <button onClick={addWork}>Add</button>
+        </div>
+      </>
+    );
+  }
+}
+
+function WorkDivs({ work, deleteWork }) {
+  const workarray = work.map((item, index) => {
+    return (
+      <div key={"w" + index} className="educationItemControls">
+        <h4>{item.company}</h4>
+
+        <button
+          className="educationDelete"
+          onClick={() => {
+            deleteWork(item.id);
+          }}
+        >
+          ❌
+        </button>
+      </div>
+    );
+  });
+
+  return <>{workarray}</>;
+}
+
+// CV Generation
 
 function CvInfo({ name, phone, email, contactAdress, paragraph }) {
   return (
@@ -215,7 +457,7 @@ function CvInfo({ name, phone, email, contactAdress, paragraph }) {
 
 function CvEducation({ education }) {
   const educationarray = education.map((item, index) => {
-    console.log(item);
+    console.log(education);
     return (
       <div key={index} className="educationItem">
         <h5>{item.yearStart + "-" + item.yearEnd}</h5>
@@ -229,16 +471,17 @@ function CvEducation({ education }) {
   return <>{educationarray}</>;
 }
 
-function EducationControlsDivs({ education }) {
-  const educationarray = education.map((item, index) => {
+function CvWork({ work }) {
+  const workarray = work.map((item, index) => {
     return (
-      <div key={index} className="educationItemControls">
-        <h4>{item.school}</h4>
-
-        <button className="educationDelete">❌</button>
+      <div key={"wcv" + index} className="educationItem">
+        <h5>{item.workStart + "-" + item.workEnd}</h5>
+        <h2>{item.company}</h2>
+        <h3>{item.position}</h3>
+        <p>{item.workDescription}</p>
       </div>
     );
   });
 
-  return <>{educationarray}</>;
+  return <>{workarray}</>;
 }
